aa7f57787d2afbb12cf83113ee30d9dd00fcaadc
Abdi Adan <abdi.adan@healthcloud.co.ke>
2021-05-02 16:18:38 +0300

    checkpoint: initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Abdi Adan | Home</title>
+                <link rel="icon" href="/favicon/favicon.ico" />
+            </Head>
+
+            <main>
+                <h1>Welcome to My Portfolio</h1>
+            </main>
+        </div>
+    );
+}
